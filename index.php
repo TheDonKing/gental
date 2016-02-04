c2e2218273f6e5519a6e0c7fbb0c173cda2e3dc7
TheDonKing <starspersona@gmail.com>
2016-02-04 14:24:51 +0300

    Добавление ИНН ОГРН

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-﻿<!DOCTYPE html>
+<!DOCTYPE html>
 <html lang="ru">
 <head>
 	<meta charset="UTF-8">
@@ -133,7 +133,8 @@
 
         </div>
         <div class="team">
-            <div id="team" class="row">
+            <div id="team" class="team showmore_content" >
+               <div class="row"> 
                 <h2>Наша <span>Команда</span></h2>
                 <ul class="ch-grid">
                     <li>
@@ -219,9 +220,11 @@
                         </div>
                     </li>
                 </ul>
+                </div>
+            </div>
                 <div class="big-logo">
                     <img src="static/img/biglogo.jpg" alt=""/>
-                </div>
+                
             </div>
         </div>
         <div class="facts">
@@ -560,9 +563,9 @@
             <p>+7 928 837 80 80</p>
             <p class="info">
                 <span class="ooo">ООО "Медпрофцентр"</span><br>
-                ИНН 7705932118<br>
+                ИНН 0562028950<br>
                 КПП 772701001<br>
-                ОГРН 1107746895442<br>
+                ОГРН 1020502624229<br>
                 Юридический и фактический адрес:<br>
                 г. Махачкала, ул. Нурадилова 94
             </p>
@@ -636,301 +639,8 @@
     <script src="static/js/custom.js"></script> <!-- не удалять -->
     <!--Modal-->
     <script src="static/js/animatedmodal/animatedModal.js"></script>
-    
     <!-- ZenaHunter модули -->
-    <script>
-    
-//Имплантация зубов
-        //demo 01
-        $("#demo01").animatedModal({
-             modalTarget:'modal-01',
-            color:'#fff',
-            animatedIn:' zoomIn',
-            animatedOut:'zoomOut',
-        });
-        
-         //demo 011
-        $("#demo011").animatedModal({
-             modalTarget:'modal-011',
-            color:'#fff',
-            animatedIn:' zoomIn',
-            animatedOut:'zoomOut',
-        });
-        
-         //demo 12
-        $("#demo012").animatedModal({
-             modalTarget:'modal-012',
-            color:'#fff',
-            animatedIn:' zoomIn',
-            animatedOut:'zoomOut',
-        });
-        
-         //demo 013
-        $("#demo013").animatedModal({
-             modalTarget:'modal-013',
-            color:'#fff',
-            animatedIn:' zoomIn',
-            animatedOut:'zoomOut',
-        });
-        
-         //demo 014
-        $("#demo014").animatedModal({
-             modalTarget:'modal-014',
-            color:'#fff',
-            animatedIn:' zoomIn',
-            animatedOut:'zoomOut',
-        });
-        
-         //demo 015
-        $("#demo015").animatedModal({
-             modalTarget:'modal-015',
-            color:'#fff',
-            animatedIn:' zoomIn',
-            animatedOut:'zoomOut',
-        });
-        
-//Исправление прикуса        
-        //demo 02
-        $("#demo02").animatedModal({
-             modalTarget:'modal-02',
-            color:'#fff',
-            animatedIn:' zoomIn',
-            animatedOut:'zoomOut',
-        });
-        
-        //demo 021
-        $("#demo021").animatedModal({
-             modalTarget:'modal-021',
-            color:'#fff',
-            animatedIn:' zoomIn',
-            animatedOut:'zoomOut',
-        });
-        
-        //demo 022
-        $("#demo022").animatedModal({
-             modalTarget:'modal-022',
-            color:'#fff',
-            animatedIn:' zoomIn',
-            animatedOut:'zoomOut',
-        });
-        
-        //demo 023
-        $("#demo023").animatedModal({
-             modalTarget:'modal-023',
-            color:'#fff',
-            animatedIn:' zoomIn',
-            animatedOut:'zoomOut',
-        });
-
-
-//Лечение десен
-        //demo 03
-        $("#demo03").animatedModal({
-             modalTarget:'modal-03',
-            color:'#fff',
-            animatedIn:' zoomIn',
-            animatedOut:'zoomOut',
-        });
-
-        //demo 031
-        $("#demo031").animatedModal({
-             modalTarget:'modal-031',
-            color:'#fff',
-            animatedIn:' zoomIn',
-            animatedOut:'zoomOut',
-        });
-        
-
-        //demo 032
-        $("#demo032").animatedModal({
-             modalTarget:'modal-032',
-            color:'#fff',
-            animatedIn:' zoomIn',
-            animatedOut:'zoomOut',
-        });
-
-
-
-//Лечение зубов        
-        //demo 04
-        $("#demo04").animatedModal({
-             modalTarget:'modal-04',
-            color:'#fff',
-            animatedIn:' zoomIn',
-            animatedOut:'zoomOut',
-        });
-
-        //demo 041
-        $("#demo041").animatedModal({
-             modalTarget:'modal-041',
-            color:'#fff',
-            animatedIn:' zoomIn',
-            animatedOut:'zoomOut',
-        });
-        
-
-        //demo 042
-        $("#demo042").animatedModal({
-             modalTarget:'modal-042',
-            color:'#fff',
-            animatedIn:' zoomIn',
-            animatedOut:'zoomOut',
-        });
-        
-
-//Протезирование зубов        
-        //demo 05
-        $("#demo05").animatedModal({
-             modalTarget:'modal-05',
-            color:'#fff',
-            animatedIn:' zoomIn',
-            animatedOut:'zoomOut',
-        });
-        
-        //demo 051
-        $("#demo051").animatedModal({
-             modalTarget:'modal-051',
-            color:'#fff',
-            animatedIn:' zoomIn',
-            animatedOut:'zoomOut',
-        });
-        
-        //demo 052
-        $("#demo052").animatedModal({
-             modalTarget:'modal-052',
-            color:'#fff',
-            animatedIn:' zoomIn',
-            animatedOut:'zoomOut',
-        });
-        
-        //demo 053
-        $("#demo053").animatedModal({
-             modalTarget:'modal-053',
-            color:'#fff',
-            animatedIn:' zoomIn',
-            animatedOut:'zoomOut',
-        });
-        
-        
-//Реставрация зубов        
-        //demo 06
-        $("#demo06").animatedModal({
-             modalTarget:'modal-06',
-            color:'#fff',
-            animatedIn:' zoomIn',
-            animatedOut:'zoomOut',
-        });
-        
-         //demo 061
-        $("#demo061").animatedModal({
-             modalTarget:'modal-061',
-            color:'#fff',
-            animatedIn:' zoomIn',
-            animatedOut:'zoomOut',
-        });
-        
-         //demo 062
-        $("#demo062").animatedModal({
-             modalTarget:'modal-062',
-            color:'#fff',
-            animatedIn:' zoomIn',
-            animatedOut:'zoomOut',
-        });
-        
-         //demo 063
-        $("#demo063").animatedModal({
-             modalTarget:'modal-063',
-            color:'#fff',
-            animatedIn:' zoomIn',
-            animatedOut:'zoomOut',
-        });
-        
-         //demo 064
-        $("#demo064").animatedModal({
-             modalTarget:'modal-064',
-            color:'#fff',
-            animatedIn:' zoomIn',
-            animatedOut:'zoomOut',
-        });
-        
-        
-//Удаление и хирургия        
-        //demo 07
-        $("#demo07").animatedModal({
-             modalTarget:'modal-07',
-            color:'#fff',
-            animatedIn:' zoomIn',
-            animatedOut:'zoomOut',
-        });
-        
-         //demo 071
-        $("#demo071").animatedModal({
-             modalTarget:'modal-071',
-            color:'#fff',
-            animatedIn:' zoomIn',
-            animatedOut:'zoomOut',
-        });
-        
-         //demo 072
-        $("#demo072").animatedModal({
-             modalTarget:'modal-072',
-            color:'#fff',
-            animatedIn:' zoomIn',
-            animatedOut:'zoomOut',
-        });
-        
-         //demo 073
-        $("#demo073").animatedModal({
-             modalTarget:'modal-073',
-            color:'#fff',
-            animatedIn:' zoomIn',
-            animatedOut:'zoomOut',
-        });
-        
-        
-//Чистка и отбеливание зубов        
-        //demo 08
-        $("#demo08").animatedModal({
-             modalTarget:'modal-08',
-            color:'#fff',
-            animatedIn:' zoomIn',
-            animatedOut:'zoomOut',
-        });
-        
-         //demo 081
-        $("#demo081").animatedModal({
-             modalTarget:'modal-081',
-            color:'#fff',
-            animatedIn:' zoomIn',
-            animatedOut:'zoomOut',
-        });
-        
-         //demo 082
-        $("#demo082").animatedModal({
-             modalTarget:'modal-082',
-            color:'#fff',
-            animatedIn:' zoomIn',
-            animatedOut:'zoomOut',
-        });
-        
-         //demo 083
-        $("#demo083").animatedModal({
-             modalTarget:'modal-083',
-            color:'#fff',
-            animatedIn:' zoomIn',
-            animatedOut:'zoomOut',
-        });
-        
-         //demo 084
-        $("#demo084").animatedModal({
-             modalTarget:'modal-084',
-            color:'#fff',
-            animatedIn:' zoomIn',
-            animatedOut:'zoomOut',
-        });
-        
-
-    </script>
-<script type="text/javascript" src="//cdn.callbackhunter.com/cbh.js?hunter_code=2e54d2d6110be80dd86a08ce7fd30664" charset="UTF-8"></script>
+    <script src="static/js/main.js"></script>
+    <script type="text/javascript" src="//2calls.ru/files/widgets/3865-4fe-79358d64e76f5a834a4f-6006afbeae-ae79358d64e76f.js" charset="UTF-8"></script>
 </body>
 </html>
--- static/js/main.js
+++ static/js/main.js
@@ -1,100 +1,296 @@
 $(document).ready(function() {
-    function addBaloon() {
-        var itemClass = "addressBox";
-        $(".ymaps-point-overlay").append('<div class="' + itemClass + '"></div>');
-        $(".addressBox").append("<p>РД, г.Махачкала, ул. Нурадилова, 94</p>");
-        $(".addressBox").append("<p>Режим работы: Пн-Сб 09:00 до 19:00</p>")
-    }
 
-    setTimeout(addBaloon, 5000);
 
-    $(".countdown").countEverest({
-        day: 31,
-        month: 12,
-        year: 2014,
-        daysLabel: "Дней",
-        dayLabel: "Дней",
-        hoursLabel: "Часов",
-        hourLabel: "Часов",
-        minutesLabel: "Минут",
-        minuteLabel: "Минут",
-        secondsLabel: "Секунд",
-        secondLabel: "Секунд",
+//Имплантация зубов
+        //demo 01
+    $("#demo01").animatedModal({
+        modalTarget:'modal-01',
+        color:'#fff',
+        animatedIn:' zoomIn',
+        animatedOut:'zoomOut',
     });
 
-    $(".countdown-2").countEverest({
-        day: 31,
-        month: 12,
-        year: 2014,
-        daysLabel: "Дней",
-        dayLabel: "Дней",
-        hoursLabel: "Часов",
-        hourLabel: "Часов",
-        minutesLabel: "Минут",
-        minuteLabel: "Минут",
-        secondsLabel: "Секунд",
-        secondLabel: "Секунд",
+    //demo 011
+    $("#demo011").animatedModal({
+        modalTarget:'modal-011',
+        color:'#fff',
+        animatedIn:' zoomIn',
+        animatedOut:'zoomOut',
     });
 
-    slidering({
-        sliderBlock:    ".slidering",
-        sliderIn:       ".inslide > div",
-        nextArr:        ".next-arr",
-        prewArr:        ".prev-arr",
-        slideAmount:    $(".slidering .inslide > div > div.item").size(),
-        slideWidth:     240,
-        duration:       600,
-        show:           2,
-        auto:           false
+    //demo 12
+    $("#demo012").animatedModal({
+        modalTarget:'modal-012',
+        color:'#fff',
+        animatedIn:' zoomIn',
+        animatedOut:'zoomOut',
     });
 
-    $('.free-services-popup-link').magnificPopup({
-        closeBtnInside:true,
-        removalDelay: 300,
-        mainClass: 'mfp-fade',
+    //demo 013
+    $("#demo013").animatedModal({
+        modalTarget:'modal-013',
+        color:'#fff',
+        animatedIn:' zoomIn',
+        animatedOut:'zoomOut',
     });
 
-    $(".slider3d .c-wrapper").carouFredSel({
-        height: 510,
-        responsiveClass: true,
-        items: {
-            visible: 3,
-            minimum: 2
-        },
-        responsive:{
-            600:{
-                visible: 2,
-                minimum: 2
-            },
-            0: {
-                visible: 2,
-                minimum: 2
-            }
-        },
-
-        scroll: 1,
-        auto: false,
-        prev: {
-            button: ".slider3d .prev-arr-2",
-            key: "left",
-            onBefore: function(){
-                $(".slider3d .c-wrapper").trigger("currentVisible", function(items) {
-                    items.removeClass("foo");
-                    items.eq(1).addClass("foo");
-                });
-            }
-        },
-        next: {
-            button: ".slider3d .next-arr-2",
-            key: "right",
-            onBefore: function(){
-                $(".slider3d .c-wrapper").trigger("currentVisible", function(items) {
-                    items.removeClass("foo");
-                    items.eq(2).addClass("foo");
-                });
-            }
-        },
-        swipe: true,
+    //demo 014
+    $("#demo014").animatedModal({
+        modalTarget:'modal-014',
+        color:'#fff',
+        animatedIn:' zoomIn',
+        animatedOut:'zoomOut',
     });
+
+    //demo 015
+    $("#demo015").animatedModal({
+        modalTarget:'modal-015',
+        color:'#fff',
+        animatedIn:' zoomIn',
+        animatedOut:'zoomOut',
+    });
+
+//Исправление прикуса
+    //demo 02
+    $("#demo02").animatedModal({
+        modalTarget:'modal-02',
+        color:'#fff',
+        animatedIn:' zoomIn',
+        animatedOut:'zoomOut',
+    });
+
+    //demo 021
+    $("#demo021").animatedModal({
+        modalTarget:'modal-021',
+        color:'#fff',
+        animatedIn:' zoomIn',
+        animatedOut:'zoomOut',
+    });
+
+    //demo 022
+    $("#demo022").animatedModal({
+        modalTarget:'modal-022',
+        color:'#fff',
+        animatedIn:' zoomIn',
+        animatedOut:'zoomOut',
+    });
+
+    //demo 023
+    $("#demo023").animatedModal({
+        modalTarget:'modal-023',
+        color:'#fff',
+        animatedIn:' zoomIn',
+        animatedOut:'zoomOut',
+    });
+
+
+//Лечение десен
+    //demo 03
+    $("#demo03").animatedModal({
+        modalTarget:'modal-03',
+        color:'#fff',
+        animatedIn:' zoomIn',
+        animatedOut:'zoomOut',
+    });
+
+    //demo 031
+    $("#demo031").animatedModal({
+        modalTarget:'modal-031',
+        color:'#fff',
+        animatedIn:' zoomIn',
+        animatedOut:'zoomOut',
+    });
+
+
+    //demo 032
+    $("#demo032").animatedModal({
+        modalTarget:'modal-032',
+        color:'#fff',
+        animatedIn:' zoomIn',
+        animatedOut:'zoomOut',
+    });
+
+
+
+//Лечение зубов
+    //demo 04
+    $("#demo04").animatedModal({
+        modalTarget:'modal-04',
+        color:'#fff',
+        animatedIn:' zoomIn',
+        animatedOut:'zoomOut',
+    });
+
+    //demo 041
+    $("#demo041").animatedModal({
+        modalTarget:'modal-041',
+        color:'#fff',
+        animatedIn:' zoomIn',
+        animatedOut:'zoomOut',
+    });
+
+
+    //demo 042
+    $("#demo042").animatedModal({
+        modalTarget:'modal-042',
+        color:'#fff',
+        animatedIn:' zoomIn',
+        animatedOut:'zoomOut',
+    });
+
+
+//Протезирование зубов
+    //demo 05
+    $("#demo05").animatedModal({
+        modalTarget:'modal-05',
+        color:'#fff',
+        animatedIn:' zoomIn',
+        animatedOut:'zoomOut',
+    });
+
+    //demo 051
+    $("#demo051").animatedModal({
+        modalTarget:'modal-051',
+        color:'#fff',
+        animatedIn:' zoomIn',
+        animatedOut:'zoomOut',
+    });
+
+    //demo 052
+    $("#demo052").animatedModal({
+        modalTarget:'modal-052',
+        color:'#fff',
+        animatedIn:' zoomIn',
+        animatedOut:'zoomOut',
+    });
+
+    //demo 053
+    $("#demo053").animatedModal({
+        modalTarget:'modal-053',
+        color:'#fff',
+        animatedIn:' zoomIn',
+        animatedOut:'zoomOut',
+    });
+
+
+//Реставрация зубов
+    //demo 06
+    $("#demo06").animatedModal({
+        modalTarget:'modal-06',
+        color:'#fff',
+        animatedIn:' zoomIn',
+        animatedOut:'zoomOut',
+    });
+
+    //demo 061
+    $("#demo061").animatedModal({
+        modalTarget:'modal-061',
+        color:'#fff',
+        animatedIn:' zoomIn',
+        animatedOut:'zoomOut',
+    });
+
+    //demo 062
+    $("#demo062").animatedModal({
+        modalTarget:'modal-062',
+        color:'#fff',
+        animatedIn:' zoomIn',
+        animatedOut:'zoomOut',
+    });
+
+    //demo 063
+    $("#demo063").animatedModal({
+        modalTarget:'modal-063',
+        color:'#fff',
+        animatedIn:' zoomIn',
+        animatedOut:'zoomOut',
+    });
+
+    //demo 064
+    $("#demo064").animatedModal({
+        modalTarget:'modal-064',
+        color:'#fff',
+        animatedIn:' zoomIn',
+        animatedOut:'zoomOut',
+    });
+
+
+//Удаление и хирургия
+    //demo 07
+    $("#demo07").animatedModal({
+        modalTarget:'modal-07',
+        color:'#fff',
+        animatedIn:' zoomIn',
+        animatedOut:'zoomOut',
+    });
+
+    //demo 071
+    $("#demo071").animatedModal({
+        modalTarget:'modal-071',
+        color:'#fff',
+        animatedIn:' zoomIn',
+        animatedOut:'zoomOut',
+    });
+
+    //demo 072
+    $("#demo072").animatedModal({
+        modalTarget:'modal-072',
+        color:'#fff',
+        animatedIn:' zoomIn',
+        animatedOut:'zoomOut',
+    });
+
+    //demo 073
+    $("#demo073").animatedModal({
+        modalTarget:'modal-073',
+        color:'#fff',
+        animatedIn:' zoomIn',
+        animatedOut:'zoomOut',
+    });
+
+
+//Чистка и отбеливание зубов
+    //demo 08
+    $("#demo08").animatedModal({
+        modalTarget:'modal-08',
+        color:'#fff',
+        animatedIn:' zoomIn',
+        animatedOut:'zoomOut',
+    });
+
+    //demo 081
+    $("#demo081").animatedModal({
+        modalTarget:'modal-081',
+        color:'#fff',
+        animatedIn:' zoomIn',
+        animatedOut:'zoomOut',
+    });
+
+    //demo 082
+    $("#demo082").animatedModal({
+        modalTarget:'modal-082',
+        color:'#fff',
+        animatedIn:' zoomIn',
+        animatedOut:'zoomOut',
+    });
+
+    //demo 083
+    $("#demo083").animatedModal({
+        modalTarget:'modal-083',
+        color:'#fff',
+        animatedIn:' zoomIn',
+        animatedOut:'zoomOut',
+    });
+
+    //demo 084
+    $("#demo084").animatedModal({
+        modalTarget:'modal-084',
+        color:'#fff',
+        animatedIn:' zoomIn',
+        animatedOut:'zoomOut',
+    });
+
+
+
 });
 
